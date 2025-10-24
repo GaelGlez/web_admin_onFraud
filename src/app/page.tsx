77c59b2eb9d232928d9dbcdcbc0028c10ff0f969
GaelGlez <gaelos.glez@gmail.com>
2025-10-24 15:19:32 -0600

    cambios

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,5 @@
-// middleware.ts
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { redirect } from "next/navigation";
 
-export function middleware(req: NextRequest) {
-  if (req.nextUrl.pathname === '/') {
-    return NextResponse.redirect(new URL('/login', req.url));
-  }
-  return NextResponse.next();
+export default function Page() {
+  redirect("/login");
 }
-
-export const config = {
-  matcher: '/',
-};
